720e5da41c359a47e169e41944bbfd372d69344c
Erick Alvarez <erick505alvarez@gmail.com>
2022-09-16 20:55:52 -0700

    adjust carousel js

--- public/javascripts/carousel.js
+++ public/javascripts/carousel.js
@@ -28,3 +28,17 @@ buttons.forEach(button => {
         delete activeText.dataset.activeText;
     })
 });
+
+// reviews revolve
+const reviewCards = document.querySelectorAll(".review-card");
+
+reviewCards.forEach(reviewCard => {
+    reviewCard.addEventListener("click", () => {
+        // select activeCard via clicked card, traverse up, 
+        const activeCard = reviewCard
+            .closest("[data-review-cards]")
+            .querySelector("[data-active-card]");
+        reviewCard.dataset.activeCard = true;
+        delete activeCard.dataset.activeCard;
+    })
+})
