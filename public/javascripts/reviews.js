1936f0f454edeb0294877ad4d208997fc52e25ba
Erick Alvarez <erick505alvarez@gmail.com>
2022-11-05 16:57:49 -0700

    complete home page yelp swiper

--- public/javascripts/reviews.js
+++ public/javascripts/reviews.js
@@ -1,13 +1,41 @@
 // reviews revolve
-const reviewCards = document.querySelectorAll(".review-card");
+// const reviewCards = document.querySelectorAll(".review-card");
 
-reviewCards.forEach(reviewCard => {
-    reviewCard.addEventListener("click", () => {
-        // select activeCard via clicked card, traverse up, 
-        const activeCard = reviewCard
-            .closest("[data-review-cards]")
-            .querySelector("[data-active-card]");
-        reviewCard.dataset.activeCard = true;
-        delete activeCard.dataset.activeCard;
-    })
+// reviewCards.forEach(reviewCard => {
+//     reviewCard.addEventListener("click", () => {
+//         // select activeCard via clicked card, traverse up, 
+//         const activeCard = reviewCard
+//             .closest("[data-review-cards]")
+//             .querySelector("[data-active-card]");
+//         reviewCard.dataset.activeCard = true;
+//         delete activeCard.dataset.activeCard;
+//     })
+// })
+
+const swiper = new Swiper(".swiper", {
+    //optional params
+    // direction: "horizontal",
+    // loop: true,
+
+    effect: "cards",
+    grabCursor: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next-unique',
+        prevEl: '.swiper-button-prev-unique',
+      },
+
+    spaceBetween: 200,
+});
+
+// dim left arrow on begginning
+swiper.on("reachBeginning", function () {
+    // select left arrow
+})
+
+// dim right arrow when reaching end
+swiper.on("reachEnd", function () {
+    // select right arrow
+
 })
