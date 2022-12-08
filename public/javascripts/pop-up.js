1c4891c2759d52e39f170c554221354ad62c6fc1
Erick Alvarez <erick505alvarez@gmail.com>
2022-12-07 21:11:12 -0800

    complete contact pop up responsiveness

--- public/javascripts/pop-up.js
+++ public/javascripts/pop-up.js
@@ -1,17 +1,21 @@
 // select nav contact button
 const contact_btn =  document.querySelector("#contact");
 const pop_up_window =  document.querySelector("#pop-up");
+const main_html = document.querySelector("html");
 
 // select close button
 const close_btn = document.querySelector("#close-btn");
 
 
-// add event listener
+// add contact button event listener
 contact_btn.addEventListener("click", () => {
     // add pop-up-show class
     pop_up_window.classList.add("pop-up-show");
     // remove pop-up-hide class
     pop_up_window.classList.remove("pop-up-hide");
+
+    // stop main page from scrolling
+    main_html.classList.add("covered");
 
     // close button animation
     close_btn.classList.add("pop-up-show");
@@ -20,12 +24,15 @@ contact_btn.addEventListener("click", () => {
 
 
 
-// add event listener
+// add close button event listener
 close_btn.addEventListener("click", () => {
     // remove pop-up-show class
     pop_up_window.classList.remove("pop-up-show");
     // add pop-up-hide class
     pop_up_window.classList.add("pop-up-hide");
+
+    // stop main page from scrolling
+    main_html.classList.remove("covered");
 
     // close button animation
     close_btn.classList.remove("pop-up-show");
